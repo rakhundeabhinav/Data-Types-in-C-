34e44544525ed17e4e509821051d9789b8d8d6d5
Abhinav Rakhunde <rakhundeabhinav@gmail.com>
2025-07-17 22:25:39 +0530

    Add files via upload

---
+++ Data Types in C++.cpp
@@ -0,0 +1,30 @@
+// C++ Program to demonstrate different data types
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float height = 5.11f;
+    double weight = 84.4;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Abhinav";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Weight: " << weight << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
+/*  OUTPUT:
+Name: Abhinav
+Age: 20
+Height: 5.11
+Weight: 84.4
+Grade: A
+Is Student: 1
+*/
